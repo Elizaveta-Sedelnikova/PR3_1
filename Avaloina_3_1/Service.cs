ef941ae4b3b8492c5c2b6c2a169d7ed13ccd8d28
T9RB <m9kish.play.tomsk@gmail.com>
2023-06-21 16:48:07 +0700

    End commit

---
+++ Avaloina_3_1/Service.cs
@@ -0,0 +1,18 @@
+﻿using Avaloina_3_1.Models;
+
+namespace Avaloina_3_1;
+
+public class Service
+{
+    private static PostgresContext? _db;
+
+    public static PostgresContext GetDbContext()
+    {
+        if (_db == null)
+        {
+            _db = new PostgresContext();
+        }
+
+        return _db;
+    }
+}
